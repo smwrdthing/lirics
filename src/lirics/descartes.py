c20050e34e33d455724b76587468d623521b0a0f
smwrdthing <146890937+smwrdthing@users.noreply.github.com>
2025-12-30 19:33:55 +0300

    move files to projects root dir

---
+++ src/lirics/descartes.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def cartesian_to_polar():
+    pass
+
+
+def polar_to_cartesian():
+    pass
+
+
+def translate():
+    pass
+
+
+def rotate():
+    pass
---
+++ src/lirics/euclid.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass, field
+from typing import Literal
+
+import numpy as np
+
+
+@dataclass
+class Geometry:
+
+    length: float
+
+    hub_radisu: float
+    rim_radius: float
+    transition_radius: float
+
+    number_of_cell: float
+    cell_angular_width: float
+
+    rim_tangent_lines_angle: float
+
+    def __post__init__(self):
+        self.cell_angular_width = 2*np.pi/self.number_of_cell
+
+        if self.transition_radius is None:  # check how to implement this better
+            self.transition_radius = self.hub_radisu
+        if self.rim_tangent_lines_angle is None:
+            self.rim_tangent_lines_angle = np.pi/2
+
+
+@dataclass
+class GeometrySingle(Geometry):
+
+    excentricity: float
+    case_raius: float
+
+
+@dataclass
+class GeometryDoubleEllipse(Geometry):
+
+    major_axis: float
+    minor_axis: float
