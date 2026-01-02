ace77f3111bfa174d6a0c0c6ac071292f42dc66f
smwrdthing <146890937+smwrdthing@users.noreply.github.com>
2026-01-02 12:35:58 +0300

    create default array of radius values for area and length computations

--- src/lirics/euclid.py
+++ src/lirics/euclid.py
@@ -24,6 +24,12 @@ class Vane(ABC):
     end_radius: float
     thickness: float
 
+    total_radius_range: NDArray[float64] = field(init=False)
+
+    def __post_init__(self):
+        self.total_radius_range = np.linspace(
+            self.start_radius, self.end_radius, NUMBER_OF_PARTITIONS)
+
     @abstractmethod
     def equation(self, radius):
         """Represents vane's midline equation, returns angular coordinate that
