5ef8f461a36f8939f22d7f72a791d53d9c4e68ea
smwrdthing <146890937+smwrdthing@users.noreply.github.com>
2026-01-04 13:41:16 +0300

    descartes content

--- src/lirics/descartes.py
+++ src/lirics/descartes.py
@@ -1,17 +1,53 @@
+from typing import Literal
 import numpy as np
+from numpy import float64
+from numpy.typing import NDArray
+
+"""2D coordinates manipulation and transformation."""
 
 
-def cartesian_to_polar():
-    pass
+def cartesian_to_polar(x: NDArray[float64], y: NDArray[float64]
+                       ) -> tuple[NDArray[float64], NDArray[float64]]:
+    return np.sqrt(x**2+y**2), np.arctan2(y, x)
 
 
-def polar_to_cartesian():
-    pass
+def polar_to_cartesian(r: NDArray[float64], phi: NDArray[float64]
+                       ) -> tuple[NDArray[float64], NDArray[float64]]:
+    return r*np.cos(phi), r*np.sin(phi)
 
 
-def translate():
-    pass
+def translate(
+        x: NDArray[float64], y: NDArray[float64],
+        shift_x: float, shift_y: float) -> tuple[NDArray[float64], NDArray[float64]]:
+    return x+shift_x, y+shift_y
 
 
-def rotate():
-    pass
+def rotate(x: NDArray[float64], y: NDArray[float64], theta: float
+           ) -> tuple[NDArray[float64], NDArray[float64]]:
+
+    x_rotated = x*np.cos(theta) - y*np.sin(theta)
+    y_rotated = x*np.sin(theta) + y*np.cos(theta)
+
+    return x_rotated, y_rotated
+
+
+def rotate_polar(r: NDArray[float64], phi: NDArray[float64], theta: float
+                 ) -> tuple[NDArray[float64], NDArray[float64]]:
+    return r, phi+theta
+
+
+def mirrox(x: NDArray[float64], y: NDArray[float64], axis: Literal["x", "y", "xy"] = "x"
+           ) -> tuple[NDArray[float64], NDArray[float64]]:
+
+    if axis not in ("x", "y", "xy"):
+        raise ValueError("Provide one of valid axis to mirror")
+
+    if axis == "x":
+        y = -y
+    elif axis == "y":
+        x = -x
+    elif axis == "xy":
+        x = -x
+        y = -y
+
+    return x, y
