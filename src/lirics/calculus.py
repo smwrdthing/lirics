720935bed304c0e240e6efccfd9eb236236ed238
smwrdthing <amiraslanov16@gmail.com>
2026-06-02 17:55:55 +0300

    useful functions for calculus

---
+++ src/lirics/calculus.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+from numpy import float64
+from numpy.typing import NDArray
+
+import numpy as np
+
+
+def function_derivative(func, x, dx):
+    return (func(x+dx)-func(x-dx))/(2*dx)
+
+
+def tabular_derivative(y, x) -> NDArray[float64]:
+    """Computes derivative with tabulated function values y corresponding
+    to points x with second order of error."""
+
+    # This is lighter than numpy's gradient, so it runs faster, thus retained.
+    #
+    # For derivative along X just pass field F and X grids as usual, for derivative
+    # along Y transpose inputs and transpose results back
+    #
+    # (just remember, this works with rows, thus all transpostitions)
+
+    dydx = np.zeros_like(x)
+
+    dydx[1:-1] = (y[2:]-y[:-2])/(x[2:]-x[:-2])
+    dydx[0] = 1/2*(-3*y[0]+4*y[1]-y[2])/(x[1]-x[0])
+    dydx[-1] = -1/2*(-3*y[-1]+4*y[-2]-y[-3])/(x[-1]-x[-2])
+
+    return dydx
+
+
+def time_derivative(y, prior_y, time_step):
+    return (y-prior_y)/time_step
+
+
+def tabular_line_integral(path, components):
+    return np.sum(np.trapezoid(components, path))
